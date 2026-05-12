9b3c94e80caa67252befa121ee8f24bbddab2b7d
DomLeng <34336728@qq.com>
2026-05-12 11:55:02 +0100

    chore: remove old React frontend

--- frontend/src/config.ts
+++
@@ -1,7 +0,0 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
-const ORDERS_URL = '/api/orders';
-const USERS_URL = '/api/users';
-const PAYPAL_URL = '/api/config/paypal';
-const STRIPE_URL = '/api/stripe';
-
-export { BASE_URL, ORDERS_URL, USERS_URL, PAYPAL_URL, STRIPE_URL };
--- frontend/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './shared';
-export * from './order';
-export * from './payment';
-export * from './user';
